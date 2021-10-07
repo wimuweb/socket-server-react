2505cacf3ff7a89b2f1eead7aa8702b0b3760f3d
Wimuweb <wimuweb@gmail.com>
2021-10-07 16:13:00 -0500

    socket server para github

--- models/server.js
+++ models/server.js
@@ -3,6 +3,7 @@ const http = require('http');
 const socketIo = require('socket.io');
 const path = require('path');
 const Sockets = require('./sockets');
+const cors = require('cors');
 
 class Server {
   constructor() {
@@ -20,6 +21,8 @@ class Server {
   middlewares() {
     // Desplegar el directorio publico
     this.app.use(express.static(path.resolve(__dirname, '../public')));
+    // CORS
+    this.app.use(cors());
   }
 
   configurarSocket() {
